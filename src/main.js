bd527762622f29b5ba6345cb2e2ce4d566f35bcb
1309155127-pixel <1309155127@qq.com>
2025-12-18 21:21:12 +0800

    杨博轩第十七次推送

--- src/main.js
+++ src/main.js
@@ -3,8 +3,16 @@ import App from '../wms-web/src/App.vue'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import './assets/global.css';
+import axios from 'axios'
+
+Vue.prototype.$axios = axios
+axios.defaults.baseURL = 'http://localhost:8090'
+axios.defaults.headers.post['Content-Type'] = 'application/json;charset=utf-8'
+axios.defaults.withCredentials = true
+
 Vue.config.productionTip = false
 Vue.use(ElementUI);
+
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
